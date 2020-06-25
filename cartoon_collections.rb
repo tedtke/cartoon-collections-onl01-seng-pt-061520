c6cbbd8c70faa1f462a7785199748d5c021f07df
Teodoro Garcia <teodorogarcia@teodoros-air.lan>
2020-06-25 16:33:37 -0500

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,50 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+
+def roll_call_dwarves(dwarf)# code an argument here
   # Your code here
+  i = 0
+
+  dwarf.each do |name|
+    i += 1
+    puts "#{i}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  # array =[]
+  # array.length > 3
+  # array.collect {|word| word + "!"}
+  #array.length = 3
+  # array = []
+  array.map {|name| name.capitalize + '!'}
+  # array.each{|name| puts name + '!'}
+  # array
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(call)# code an argument here
   # Your code here
+  #all.any? {|word| word.length >= 3}
+  call.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  #cheese.each{|x| if xinclude?(cheese_types) puts (cheese_types)}
+  cheese.find do |this_cheese| 
+    cheese_types.include? (this_cheese)
+
+    # def find_the_cheese(potentially_cheesy_items)
+    #   cheeses = %w[gouda cheddar camembert]
+    
+    #   potentially_cheesy_items.find do |maybe_cheese|
+    #     cheeses.include?(maybe_cheese)
+    #   end
+    # end
+    
+    # find_the_cheese(potentially_cheesy_items)
+  end
 end
+
+
